62bd01beb96f1aaf8290c98d6b6317b00cb5d1ba
Alex <alexmh287@gmail.com>
2018-03-20 18:35:03 -0700

    star wars game almost complete

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -2,22 +2,39 @@ var gameState = {
 	status: "start"
 }
 
-
+//xp health ex:150, 
+//xp damage adjusted xp
 var bobafett = {
 	hero_id:"bobafett",
-	role: ""
+	role: "",
+	startingHp: 150,
+	currentHp: 150,
+	attackPower: 25,
+	name: "Boba Fett"
 };
 var darth = {
 	hero_id:"darth",
-	role: ""
+	role: "",
+	startingHp: 200,
+	currentHp: 200,
+	attackPower: 35,
+	name: "Darth Vader"
 };
 var solo = {
 	hero_id:"solo",
-	role: ""
+	role: "",
+	startingHp: 80,
+	currentHp: 80,
+	attackPower: 10,
+	name: "Han Solo"
 };
 var yoda = {
 	hero_id:"yoda",
-	role: ""
+	role: "",
+	startingHp: 180,
+	currentHp: 180,
+	attackPower: 28,
+	name: "Yoda"
 };
 
 var players = {
@@ -39,26 +56,44 @@ function setRole(player,role) {
 
 }
 
+function findPlayerByRole(role) {
+	for (var pl in players) {
+		var player = players[pl];
+		if (player.role === role) { 
+			return player; 
+		} 
+	}
+
+}
+
 function drawPlayers() {
 	for (var pl in players) {
 		var player = players[pl];
 		var playerNode = $("#" + pl);
-		if(true) {
-			if(player.role === "attacker") {
-				$("#attacker").append(playerNode);
-			}
-			else if(player.role === "") { 
-				$("#attacker").append(playerNode);
-			}
-			else if(player.role === "middle") {
-				$("#middle").append(playerNode);
-			}
-			else if(player.role === "defender") {
-				$("#defender").append(playerNode);
-				$("#attack").show();
-				playerNode.append($("#attack"));
-			}
+		var hpNode = $(".hp", playerNode);
+		hpNode.html(player.currentHp);
+		if(player.role === "attacker") {
+			$("img", playerNode).css("background", "white");
+			$("#attacker").append(playerNode);
+			
 		}
+		else if(player.role === "") { 
+			$("#start").append(playerNode);
+		}
+		else if(player.role === "middle") {
+			$("img", playerNode).css("background", "red");
+			$("#middle").append(playerNode);
+		}
+		else if(player.role === "defender") {
+			$("#defender").append(playerNode);
+			$("#attack").show();
+			$("img", playerNode).css("background", "black");
+			playerNode.append($("#attack"));
+		}
+		else if(player.role === "lost") {
+			playerNode.hide();
+		}
+		
 	}
 
 }
@@ -88,7 +123,13 @@ function startGame() {
 		else if (gameState.status == "middle") {
 			if (player.role != "attacker") {
 				player.role = "defender";
-				gameState.status = "defender";
+				gameState.status = "play";
+				drawPlayers();
+			}
+		}
+		else if(gameState.status == "play") {
+			if (player.role != "attacker" && player.role != "lost") {
+				player.role = "defender";
 				drawPlayers();
 			}
 		}
@@ -100,7 +141,26 @@ function startGame() {
 	}
 
 	$("#attack").click(function(){
-		alert("lightsaber attack");
+		//alert("lightsaber attack");
+		var attacker = findPlayerByRole("attacker");
+		var defender = findPlayerByRole("defender");
+		var defenderAttackPower = defender.attackPower; 
+		var attackerAttackPower = attacker.attackPower;
+		defender.currentHp -= attackerAttackPower;
+		attacker.currentHp -= defenderAttackPower;
+
+		var strength = Math.floor((Math.random() * 10) + 10);
+		attacker.attackPower += strength; 
+
+		if (defender.currentHp < 0) {
+			defender.role = "lost";
+			$("#status").html("You defeated " + defender.name + ". You can now choose another enemy to fight.");
+		}
+		drawPlayers();
+
+
+
+
 	})
 
 	
